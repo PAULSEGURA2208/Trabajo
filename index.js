265cb3699521bc9b9bada2c7fe64d82f2d7ba7b5
PAULSEGURA2208 <paulsegura092021@gmail.com>
2024-11-27 22:19:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,127 +1,116 @@
 const btnCart = document.querySelector('.container-cart-icon');
-const containerCartProducts = document.querySelector(
-	'.container-cart-products'
-);
+const containerCartProducts = document.querySelector('.container-cart-products');
 
 btnCart.addEventListener('click', () => {
-	containerCartProducts.classList.toggle('hidden-cart');
+    containerCartProducts.classList.toggle('hidden-cart');
 });
 
-/* ========================= */
+/* ============================== */
 const cartInfo = document.querySelector('.cart-product');
 const rowProduct = document.querySelector('.row-product');
 
 // Lista de todos los contenedores de productos
 const productsList = document.querySelector('.container-items');
 
-// Variable de arreglos de Productos
+// Variable de arreglos de productos 
 let allProducts = [];
 
 const valorTotal = document.querySelector('.total-pagar');
 
-const countProducts = document.querySelector('#contador-productos');
+const countProducts = document.querySelector('#contador-productos')
 
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
 
 productsList.addEventListener('click', e => {
-	if (e.target.classList.contains('btn-add-cart')) {
-		const product = e.target.parentElement;
 
-		const infoProduct = {
-			quantity: 1,
-			title: product.querySelector('h2').textContent,
-			price: product.querySelector('p').textContent,
-		};
+    if(e.target.classList.contains('btn-add-cart')){
+        const product = e.target.parentElement
 
-		const exits = allProducts.some(
-			product => product.title === infoProduct.title
-		);
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h2').textContent,
+            price: product.querySelector('p').textContent,
+        };
 
-		if (exits) {
-			const products = allProducts.map(product => {
-				if (product.title === infoProduct.title) {
-					product.quantity++;
-					return product;
-				} else {
-					return product;
-				}
-			});
-			allProducts = [...products];
-		} else {
-			allProducts = [...allProducts, infoProduct];
-		}
+        const exits = allProducts.some(product => product.title == infoProduct.title)
+        if (exits){
+            const products = allProducts.map(product =>{
+                if(product.title === infoProduct.title){
+                    product.quantity++;
+                    return product
+                } else {
+                    return product
+                }
+            })
+            allProducts = [...products]
+        } else{
+            allProducts = [...allProducts, infoProduct];
+        }
 
-		showHTML();
-	}
+        showHTML();
+
+    }
+
 });
 
-rowProduct.addEventListener('click', e => {
-	if (e.target.classList.contains('icon-close')) {
-		const product = e.target.parentElement;
-		const title = product.querySelector('p').textContent;
+rowProduct.addEventListener('click', (e) => {
+    if(e.target.classList.contains('icon-close')){
+        const product = e.target.parentElement;
+        const title = product.querySelector('p').textContent;
 
-		allProducts = allProducts.filter(
-			product => product.title !== title
-		);
+        allProducts = allProducts.filter(
+            product => product.title !== title
+        );
 
-		console.log(allProducts);
-
-		showHTML();
-	}
+        console.log(allProducts);
+        showHTML();
+    }
 });
 
-// Funcion para mostrar  HTML
+//Funcion para mostrar HTML
 const showHTML = () => {
-	if (!allProducts.length) {
-		cartEmpty.classList.remove('hidden');
-		rowProduct.classList.add('hidden');
-		cartTotal.classList.add('hidden');
-	} else {
-		cartEmpty.classList.add('hidden');
-		rowProduct.classList.remove('hidden');
-		cartTotal.classList.remove('hidden');
-	}
 
-	// Limpiar HTML
-	rowProduct.innerHTML = '';
+    if(!allProducts.length){
+        containerCartProducts.innerHTML=`
+        <p class="cart-empty">El carrito está vacío</p>
+        `
+    };
 
-	let total = 0;
-	let totalOfProducts = 0;
+    //Limpiar HTML
+    rowProduct.innerHTML ='';
 
-	allProducts.forEach(product => {
-		const containerProduct = document.createElement('div');
-		containerProduct.classList.add('cart-product');
+    let total = 0;
+    let totalOfProducts = 0;
 
-		containerProduct.innerHTML = `
-            <div class="info-cart-product">
-                <span class="cantidad-producto-carrito">${product.quantity}</span>
-                <p class="titulo-producto-carrito">${product.title}</p>
-                <span class="precio-producto-carrito">${product.price}</span>
-            </div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon-close"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                />
-            </svg>
-        `;
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('cart-product');
 
-		rowProduct.append(containerProduct);
+        containerProduct.innerHTML = `
+        <div class="info-cart-product">
+							<span class="cantidad-producto-carrito">${product.quantity}</span>
+							<p class="titulo-producto-carrito">${product.title}</p>
+							<span class="precio-producto-carrito">${product.price}</span>
+						</div>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-close">
+							<path 
+							   stroke-linecap="round" 
+							   stroke-linejoin="round" 
+							   d="M6 18 18 6M6 6l12 12" 
+							/>
+						  </svg>
+        
+        `
 
-		total =
-			total + parseInt(product.quantity * product.price.slice(1));
-		totalOfProducts = totalOfProducts + product.quantity;
-	});
+        rowProduct.append(containerProduct);
 
-	valorTotal.innerText = `$${total}`;
-	countProducts.innerText = totalOfProducts;
+        total = total + parseInt(product.quantity * product.price.slice(1));
+        totalOfProducts = totalOfProducts + product.quantity;
+
+
+    });
+
+    valorTotal.innerText = `$${total}`;
+    countProducts.innerText = totalOfProducts;
+
+
 };
